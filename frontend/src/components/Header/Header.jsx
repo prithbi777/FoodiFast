873b9d02d4d6291bdf0c3696357d3a5e1a80b674
prithbi777 <prithbirajmahanta@gmail.com>
2025-07-21 17:53:12 +0530

    header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <div className='header'>
         <div className="header-contents">
 
-            <h2>Order your favourite food here!</h2>
+            <h2>Hurry Up!</h2>
             <p>Choose from the best collection of food which are made by love. This is the paradise of food presents all you need right now so why are you waiting for?</p>
             <button>View Menu</button>
 
